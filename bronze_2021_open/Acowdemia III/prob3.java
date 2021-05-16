26cea22fec7e127ffcff01f4bda9ac509c68f2c4
Yitong Luo <yitong041115@163.com>
2021-05-16 18:18:16 +0800

    Success

--- bronze_2021_open/Acowdemia III/prob3.java
+++ bronze_2021_open/Acowdemia III/prob3.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.*;
-
 class prob3
 {
     public static void main(String[] args) throws IOException
@@ -16,19 +15,20 @@ class prob3
         for (int i = 1; i < row + 1; i++)
         {
             StringTokenizer a = new StringTokenizer(in.readLine());
+            String line = a.nextToken();
             for (int j = 1; j < col + 1; j++)
             {
-                String current = a.nextToken();
-                if (current.equals("C")) 
+                char current = line.charAt(j - 1);
+                if (current == 'C') 
                 {
                     grassland[i][j] = -1;
                     construct(grassland, i, j);
                 }   
-                if (current.equals("."))    
+                if (current == '.')    
                 {
                     grassland[i][j] = -2;
                 }     
-                else
+                if (current == 'G')
                 {
                     xcoorgrass[grasscount] = i;
                     ycoorgrass[grasscount] = j;
@@ -37,10 +37,38 @@ class prob3
                 //cow is -1, . is -2, grass is the number with cow adjacent
             }
         }
-        for (int a = 1; a < row + 1; a++)
+        // for (int a = 0; a < row + 1; a++)
+        // {
+        //     System.out.println(Arrays.toString(grassland[a]));
+        // }
+        //System.out.println(Arrays.toString(xcoorgrass));
+        //System.out.println(Arrays.toString(ycoorgrass));
+        System.out.println(count(grassland, xcoorgrass, ycoorgrass, grasscount));
+    }
+
+    public static int count(int[][] mat, int[]a, int[] b, int count)
+    {
+        int result = 0;
+        int conflict = 0;
+        for (int i = 1; i <= count; i++)
         {
-            System.out.println(Arrays.toString(grassland[a]));
+            if (mat[a[i]][b[i]] >= 3)
+            {
+                result++;
+            }
+            if (mat[a[i]][b[i]] == 2)
+            {
+                if (isConflict(mat, a[i], b[i]))
+                {
+                    conflict++;
+                }
+                else
+                {
+                    result++;
+                }
+            }
         }
+        return result + conflict/2;
     }
 
     public static int[][] construct(int[][] input, int x, int y)
@@ -62,5 +90,34 @@ class prob3
             input[x][y + 1]++;
         }
         return input;
+    }
+    
+    public static boolean isConflict(int[][] mat, int x, int y)
+    {
+
+        //int up = mat[x - 1][y];
+        //int down = mat[x + 1][y];
+        //int left = mat[x][y - 1];
+        //int right = mat[x][y + 1];
+        if (x - 1 >= 1 && y - 1 >= 1 && mat[x - 1][y - 1] == 2 && mat[x - 1][y] == -1 && mat[x][y - 1] == -1)
+        {
+            return true;
+        }
+        if (x - 1 >= 1 && y + 1 < mat[0].length && mat[x - 1][y + 1] == 2 &&  mat[x - 1][y] == -1 
+        && mat[x][y + 1] == -1)
+        {
+            return true;
+        }
+        if (x + 1 < mat.length && y - 1 >= 1 && mat[x + 1][y - 1] == 2 && mat[x + 1][y] == -1 
+        &&  mat[x][y - 1] == -1)
+        {
+            return true;
+        }
+        if (x + 1 < mat.length && y + 1 < mat[0].length && mat[x + 1][y + 1] == 2 && mat[x + 1][y] == -1 
+        && mat[x][y + 1] == -1)
+        {
+            return true;
+        }
+        return false;
     }
 }
